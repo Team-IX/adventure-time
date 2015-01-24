0ddcd867ca01f84b7aee503bd3b54cddd5e84e6c
Garion Herman <cheddam@me.com>
2015-01-24 16:06:39 +1300

    Progress on the web interface.

--- web/js/app.js
+++ web/js/app.js
@@ -3,10 +3,24 @@
 var Player = {
 	number: 0,
 	id: 0,
-	color: '#000000'
+	color: 'fuchsia'
+};
+
+var buttonsPressed = {
+	up: 0,
+	left: 0,
+	right: 0,
+	down: 0,
+	bomb: 0
 };
 
 $(document).ready(function ()
+{
+	// initWithServer();
+	initWithoutServer(); // Remove when actually running game
+});
+
+function initWithServer()
 {
 	// Request a number / color
 	$.get({
@@ -22,19 +36,72 @@ $(document).ready(function ()
 			startGame();
 		}
 	});
-	$('.control').on('mousedown mouseup', updateKeys);
-});
+}
+
+function initWithoutServer()
+{
+	Player.number = 1;
+	Player.id = 1;
+	Player.color = 'fuchsia';
+
+	$('.loading').hide();
+
+	startGame();
+}
 
 function startGame()
 {
 	// Set background color and shit
-	$('.bomb').css('control', Player.color);
-	$('.bomb').text('Player ' + Player.playerNumber);
+	$('.control').css({ background: Player.color });
+	$('#bomb').text('Player ' + Player.number);
 
 	// Set up event handlers
+	$('.control').on('mouseup mousedown', function(event) {
+		event.preventDefault();
+		updateKeys(event);
+	});
 }
 
 function updateKeys(event)
 {
-	console.log(event);
+	// Check which button changed state, and what action occurred
+	changedButton = event.target.id;
+	newState = event.type;
+
+	// Update the buttonsPressed object
+	if (newState == 'mousedown') {
+		buttonsPressed[changedButton] = 1;
+	} else {
+		buttonsPressed[changedButton] = 0;
+	}
+
+	updateServer();
+}
+
+function updateServer()
+{
+	var buttonsToSend = [];
+	var buttonKeys = Object.keys(buttonsPressed);
+
+	// Generate list of buttons to send
+	for (var i = buttonKeys.length - 1; i >= 0; i--) {
+		console.log(buttonsPressed);
+		if (buttonsPressed[buttonKeys[i]] == 1) {
+			buttonsToSend.push(buttonKeys[i]);
+		}
+	};
+
+	console.log(buttonsToSend);
+
+	$.get({
+		url: '/update',
+		data: {
+			id: Player.id,
+			controls: buttonsToSend
+		},
+		success: function(data)
+		{
+			// SHIT SON
+		}
+	});
 }
